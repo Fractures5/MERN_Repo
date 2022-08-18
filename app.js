b3735e4f150193573197d0256bfdb4309885f3a5
Faaris Khan <tdv2188@autuni.ac.nz>
2022-08-18 21:43:28 +1200

    Testing deployment of Heroku

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
 // app.js
-
+require('dotenv').config({ path: "./env"});
 const express = require('express');
 const connectDB = require('./config/db');
 var cors = require('cors');
-
-// routes
 const books = require('./routes/api/books');
-
 const app = express();
+const path =  require('path');
+const { response } = require('express');
 
-// Connect Database
 connectDB();
 
 // cors
 app.use(cors({ origin: true, credentials: true }));
-
-// Init Middleware
 app.use(express.json({ extended: false }));
-
-app.get('/', (req, res) => res.send('Hello world!'));
-
-// use Routes
 app.use('/api/books', books);
 
-const port = process.env.PORT || 8082;
+app.use(express.static(path.join(__dirname, "./MERN_Repo/build")))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "MERN_Repo", "build", "index.html"))
+})
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
